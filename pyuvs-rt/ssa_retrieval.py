55001b2e5405f8dd99b26b3c7ff596b0ba3f94eb
kyle <kdconey1@gmail.com>
2021-02-22 13:22:45 -0700

    Final commit before adding parallelization

--- pyuvs-rt/ssa_retrieval.py
+++ pyuvs-rt/ssa_retrieval.py
@@ -4,12 +4,12 @@
 import os
 import time
 from tempfile import mkdtemp
+import multiprocessing as mp
 
 # 3rd-party imports
 import numpy as np
 from astropy.io import fits
 from scipy import optimize
-#import joblib
 
 # Local imports
 import disort
@@ -27,7 +27,6 @@ from pyRT_DISORT.untested.vertical_profiles import Conrath
 from pyRT_DISORT.untested.phase_function import TabularLegendreCoefficients
 from pyRT_DISORT.untested.rayleigh import RayleighCo2
 from pyRT_DISORT.untested.model_atmosphere import ModelAtmosphere
-#from pyRT_DISORT.preprocessing.utilities.shared_array import SharedArray
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Read in external aerosol files
@@ -95,7 +94,7 @@ usrtau = mb.user_optical_depths
 # Observation
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # TODO: This is just a test file. Select the data I want here
-files = ['/home/kyle/Downloads/mvn_iuv_l1c_apoapse-orbit07200-muv_20180611T170741_v13_r01.fits']
+files = ['/home/kyle/Downloads/mvn_iuv_l1c_apoapse-orbit07287-muv_20180627T204223_v13_r01.fits']
 for file in files:
     hdul = fits.open(file)
     reflectance = hdul['reflectance'].data
@@ -115,12 +114,20 @@ for file in files:
     sza = hdul['solar_zenith_angle'].data
     emission_angle = hdul['emission_angle'].data
     phase_angle = hdul['phase_angle'].data
+    lt = hdul['local_time'].data
     
     angles = Angles(sza, emission_angle, phase_angle)
     mu = angles.mu
     mu0 = angles.mu0
     phi = angles.phi
     phi0 = angles.phi0
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Make a shared array
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    memmap_filename = os.path.join(mkdtemp(), 'myNewFile.dat')
+    retrieved_ssas = np.memmap(memmap_filename, dtype=np.float, shape=reflectance.shape,
+                               mode='w+')
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Make the size of the computational parameters
@@ -149,6 +156,7 @@ for file in files:
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Loop over pixel
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    t0 = time.time()
     for integration in range(n_integrations):
         for position in range(n_positions):
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -163,15 +171,16 @@ for file in files:
             rco2 = RayleighCo2(short_wav[integration, position, :], model_eos,
                                n_moments)
 
+            answer = np.zeros(n_wavelengths)
             for wavelength in range(n_wavelengths):
                 def fit_ssa(guess, wav):
                     # Trap the guess
                     if not 0 <= guess <= 1:
                         return 9999999
                     cext = dust_file.array['primary'].data[:, :, 0]
-                    cext[:, :] = 1
+                    cext[:, 0:2] = 1  # TODO: are these indices correct?
                     csca = dust_file.array['primary'].data[:, :, 1]
-                    csca[:, :] = guess  # TODO: or whatever
+                    csca[:, 0:2] = guess  # TODO: are these indices correct?
                     c_ext = ForwardScatteringProperty(
                         cext,
                         particle_size_grid=sizes,
@@ -185,11 +194,11 @@ for file in files:
                     dust_properties.add_property(c_ext, 'c_extinction')
                     dust_properties.add_property(c_sca, 'c_scattering')
 
-                    # Make the new Column where wave_ref = 9.3 microns and column OD = 1
+                    # Make the new Column where wave_ref = 9.3 microns and column OD = 7
                     # TODO: use real Curiosity OD
                     dust_col = Column(dust_properties, model_eos,
                                       conrath_profile.profile, p_sizes,
-                                      short_wav[integration, position, :], 9.3, 1,
+                                      short_wav[integration, position, :], 9.3, 7,
                                       dust_phsfn)
 
                     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -247,107 +256,23 @@ for file in files:
                                       albedo, btemp, ttemp, temis, radius, h_lyr, rhoq, rhou,
                                       rho_accurate, bemst, emust, accur, header, rfldir,
                                       rfldn, flup, dfdt, uavg, uu, albmed, trnmed)
-
-                    #print(uu[0, 0, 0])   # shape: (1, 81, 1)
-                    #raise SystemExit(9)
                     return (uu[0, 0, 0] - reflectance[integration, position, wav])**2
-                print(fit_ssa(0.61, 0))
-                raise SystemExit(9)
+                fitted_ssa = optimize.minimize(fit_ssa, np.array([0.6]), wavelength, method='Nelder-Mead').x
+                answer[wavelength] = fitted_ssa
+            retrieved_ssas[position, integration, :] = answer
+            t1 = time.time()
+            print(t1 - t0)
+            print(answer)
+            raise SystemExit(9)
 
-
-'''# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# These are all calculations I have to re-do for each time I try a new parameter
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# THIS USES MULTIPROCESSING MODULE
-# Make an array in shared memory
-junkThing = np.zeros((len(szas), len(wavs)), dtype=np.float)
-shm = shared_memory.SharedMemory(name='shared_answer', create=True, size=junkThing.nbytes)
-answer = np.ndarray(junkThing.shape, dtype=junkThing.dtype, buffer=shm.buf)
-answer[:] = junkThing[:]
-
-# THIS USES JOBLIB MODULE
-#filename = os.path.join(mkdtemp(), 'myNewFile.dat')
-#answer = np.memmap(filename, dtype=np.float, shape=(len(szas), len(wavs)), mode='w+')
-
-# Use my code to create an array in shared memory
-sa = SharedArray((len(szas), len(wavs)))
-answer = sa.array
-
-
-def myModel(guess, pixel):
-    dust_od = guess[0]
-    ice_od = guess[1]
-    if dust_od <= 0 or ice_od <= 0:
-        return 99999
-    # The column will change everytime I use a different OD
-    dust_column = Column(dust, lay, dust_conrath, np.array([1]), np.array([dust_od]))
-    ice_column = Column(ice, lay, ice_profile, np.array([2]), np.array([ice_od]))
-
-    # The phase functions change as columns change
-    dust_phase = TabularLegendreCoefficients(dust_column, dust_phase_function.array,
-                                             particle_sizes=dust_phase_radii.array, wavelengths=dust_phase_wavs.array)
-    ice_phase = TabularLegendreCoefficients(ice_column, ice_coeff.array)
-
-    # Make the model
-    model = ModelAtmosphere()
-    dust_info = (dust_column.hyperspectral_total_optical_depths, dust_column.hyperspectral_scattering_optical_depths,
-                 dust_phase.coefficients)
-    ice_info = (ice_column.hyperspectral_total_optical_depths, ice_column.hyperspectral_scattering_optical_depths,
-                ice_phase.coefficients)
-
-    # Add everything to the model
-    model.add_constituent(dust_info)
-    model.add_constituent(ice_info)
-    model.add_constituent(rayleigh_info)
-
-    # Once everything is in the model, compute the model
-    model.compute_model()
-    optical_depths = model.hyperspectral_total_optical_depths
-    ssa = model.hyperspectral_total_single_scattering_albedos
-    polynomial_moments = model.hyperspectral_legendre_moments
-
-    #existing_shm = shared_memory.SharedMemory(name='shared_answer')
-    #shared_array = np.ndarray((len(szas), len(wavs)), dtype=np.float, buffer=existing_shm.buf)
-    for w in range(len(wavs)):
-        # The 0s here are just me testing this on a single pixel
-        junk, junk, junk, junk, junk, uu, junk, junk = disort.disort(usrang, usrtau, ibcnd, onlyfl, prnt, plank,
-             lamber, deltamplus, do_pseudo_sphere, optical_depths[:, w], ssa[:, w], polynomial_moments[:, :, w], temperatures,
-             low_wavenumber[w], high_wavenumber[w], utau, umu0[pixel], phi0, umu[pixel], phi[pixel], fbeam, fisot, albedo,
-             surface_temp, top_temp, top_emissivity, planet_radius, h_lyr, hapke.rhoq, hapke.rhou, hapke.rho_accurate[:, pixel],
-             hapke.bemst, hapke.emust, accur, header, direct_beam_flux, diffuse_down_flux,
-             diffuse_up_flux, flux_divergence, mean_intensity, intensity[:, :, pixel], albedo_medium, transmissivity_medium)
-        # This is what I'd do for multiprocessing
-        #shared_array[pixel, w] = uu[0, 0]
-        answer[pixel, w] = uu[0, 0]
-
-
-def calculate_model_difference(guess, pixel):
-    # This is just a sample spectrum I took... each pixel should have its own
-    target = np.array([0.784799E-01, 0.753058E-01, 0.698393E-01, 0.654867E-01, 0.605224E-01, 0.572824E-01, 0.534369E-01,
-                       0.496474E-01, 0.464914E-01, 0.427855E-01, 0.429494E-01, 0.457046E-01, 0.491979E-01, 0.495246E-01,
-                       0.457136E-01, 0.433431E-01, 0.483898E-01, 0.504774E-01, 0.457426E-01])
-    test_run = myModel(guess, pixel)
-    return np.sum((test_run - target)**2)
-
-
-def do_optimization(guess, pixel):
-    return optimize.minimize(calculate_model_difference, np.array(guess), pixel, method='Nelder-Mead').x
-
-
-# Without this pool won't cooperate
 if __name__ == '__main__':
-    t0 = time.time()
-    n_cpus = 8  # mp.cpu_count()
-    pixel_inds = np.linspace(0, len(szas)-1, num=len(szas), dtype='int')
-    #iterable = [[[0.8, 0.2], f] for f in pixel_inds]
+    n_cpus = mp.cpu_count()    # = 8 for my desktop, 12 for my laptop
+    pool = mp.Pool(n_cpus - 1)   # save one just to be safe. Some say it's faster too
 
-    #m = joblib.Parallel(n_jobs=-2, prefer='processes')(joblib.delayed(myModel)([0.8, 0.2], f) for f in pixel_inds)
-    joblib.Parallel(n_jobs=-2, prefer='processes')(joblib.delayed(myModel)([0.8, 0.2], f) for f in pixel_inds)
-    print(np.amax(answer))
-    #np.save('/home/kyle/bestArray.npy', answer)
-    #sa.delete()
-    t1 = time.time()
-    print(t1 - t0)'''
+
+
+
+
+    # m = joblib.Parallel(n_jobs=-2, prefer='processes')(joblib.delayed(myModel)([0.8, 0.2], f) for f in pixel_inds)
+    joblib.Parallel(n_jobs=-2, prefer='processes')(
+        joblib.delayed(myModel)([0.8, 0.2], f) for f in pixel_inds)
